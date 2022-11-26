53521788806d36e959eaedbcbcff7f78c9d822c7
Dennis Rapoport <dennis.rapoport@gmail.com>
2022-11-26 09:47:36 +0200

    created unicfae template

--- part01/part1/src/App.js
+++ part01/part1/src/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react'
+
 const App = () => {
-  const now = new Date()
-  const a = 10
-  const b = 21
-  return(
-  <div>
-    <p>Hello World!</p>
-    <p>It is {now.toString()}</p>
-    <p>{a} plus {b} is {a+b}</p>
-  </div>
+  const [value, setValue] = useState(10)
+  
+  const setToValue = (newValue) => () => {
+    console.log('value now', newValue)  // print the new value to console
+    setValue(newValue)
+  }
+  
+  return (
+    <div>
+      {value}
+      <button onClick={setToValue(1000)}>thousand</button>
+      <button onClick={setToValue(0)}>reset</button>
+      <button onClick={setToValue(value + 1)}>increment</button>
+    </div>
   )
 }
 
