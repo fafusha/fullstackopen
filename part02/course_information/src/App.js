e46fc52867e6a05a98ac84111fd362ca4dbd19eb
Dennis Rapoport <dennis.rapoport@gmail.com>
2022-11-27 22:15:54 +0200

    2.1 begin

--- part02/course_information/src/App.js
+++ part02/course_information/src/App.js
@@ -1,51 +1,27 @@
-const Header = (props) => (
-    <h1>{props.course.name}</h1>
-)
-
-const Content = (props) => (
-  <>
-    <Part part={props.course.parts[0]} />
-    <Part part={props.course.parts[1]} />
-    <Part part={props.course.parts[2]} />
-  </>
-)
-
-const Part = (props) => (
-    <p>{props.part.name} {props.part.exercises}</p>
-)
-
-const Total = (props) => {
-    const total = props.course.parts[0].exercises + props.course.parts[1].exercises + props.course.parts[2].exercises
-    return (<p>Number of exercises {total}</p>)
-}
-
-
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 export default App
